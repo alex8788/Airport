84086ea689e7f3826ac1e2fc6975798a722e9329
alex8788 <kingdom0987123@gmail.com>
2026-05-26 23:11:43 +0800

    為 Enum 加入屬性 maxBaggageWeight(載重上限)、建構子與 Getter 方法。

--- model/CabinClass.java
+++ model/CabinClass.java
@@ -1,7 +1,20 @@
 package model;
 
-public enum CabinClass {
-    ECONOMY,
-    BUSINESS,
-    FIRST
+public enum CabinClass
+{
+    ECONOMY(20.0),  // 經濟艙限重 20.0 kg
+    BUSINESS(30.0),  // 商務艙限重 30.0 kg
+    FIRST(40.0);  // 頭等艙限重 40.0 kg
+
+    private final double maxBaggageWeight;
+
+    CabinClass(double maxBaggageWeight)
+    {
+        this.maxBaggageWeight = maxBaggageWeight;
+    }
+
+    public double getMaxBaggageWeight()
+    {
+        return maxBaggageWeight;
+    }
 }
